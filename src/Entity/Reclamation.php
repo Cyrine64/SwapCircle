d120fcddc0eed017806331f6e8f14b470a493656
yassinesaoud <yassin.saoud@esprit.tn>
2025-02-14 13:42:31 +0100

    dernier version

---
+++ src/Entity/Reclamation.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReclamationRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ReclamationRepository::class)]
+#[ORM\Table(name: "reclamation")]
+class Reclamation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id_reclamation = null;
+
+    #[ORM\ManyToOne(targetEntity: Utilisateur::class)]
+    #[ORM\JoinColumn(name: "id_utilisateur", referencedColumnName: "id_utilisateur", nullable: false)]
+    private ?Utilisateur $utilisateur = null;
+
+    #[ORM\ManyToOne(targetEntity: Objet::class)]
+    #[ORM\JoinColumn(name: "id_objet", referencedColumnName: "id_objet", nullable: false)]
+    private ?Objet $objet = null;
+
+    #[ORM\Column(type: 'text')]
+    private ?string $message = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $statut = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $type_reclamation = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $date_reclamation = null;
+
+    // Getters and setters
+    public function getIdReclamation(): ?int
+    {
+        return $this->id_reclamation;
+    }
+
+    public function getUtilisateur(): ?Utilisateur
+    {
+        return $this->utilisateur;
+    }
+
+    public function setUtilisateur(?Utilisateur $utilisateur): self
+    {
+        $this->utilisateur = $utilisateur;
+        return $this;
+    }
+
+    public function getObjet(): ?Objet
+    {
+        return $this->objet;
+    }
+
+    public function setObjet(?Objet $objet): self
+    {
+        $this->objet = $objet;
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): self
+    {
+        $this->statut = $statut;
+        return $this;
+    }
+
+    public function getTypeReclamation(): ?string
+    {
+        return $this->type_reclamation;
+    }
+
+    public function setTypeReclamation(string $type_reclamation): self
+    {
+        $this->type_reclamation = $type_reclamation;
+        return $this;
+    }
+
+    public function getDateReclamation(): ?\DateTimeInterface
+    {
+        return $this->date_reclamation;
+    }
+
+    public function setDateReclamation(\DateTimeInterface $date_reclamation): self
+    {
+        $this->date_reclamation = $date_reclamation;
+        return $this;
+    }
+} 
