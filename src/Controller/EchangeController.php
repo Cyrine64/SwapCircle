e17d81841bc68916860971dcd86f306195bb23f6
Yassine Saoud <yassin.saoud@esprit.tn>
2025-03-05 00:10:34 +0100

    Echange

--- src/Controller/EchangeController.php
+++ src/Controller/EchangeController.php
@@ -7,7 +7,6 @@ use App\Entity\Objet;
 use App\Entity\Utilisateur;
 use App\Form\EchangeType;
 use App\Repository\EchangeRepository;
-use App\Service\NotificationService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,64 +19,64 @@ class EchangeController extends AbstractController
     #[Route('/', name: 'app_echange_index', methods: ['GET'])]
     public function index(EchangeRepository $echangeRepository): Response
     {
-        return $this->render('back_office/echanges/index.html.twig', [
+        return $this->render('echange/index.html.twig', [
             'echanges' => $echangeRepository->findAll(),
         ]);
     }
 
     #[Route('/new/{id}', name: 'app_echange_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, Objet $objet, NotificationService $notificationService): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, Objet $objet): Response
     {
         // Récupérer l'utilisateur (temporairement ID 1)
         $user = $entityManager->getRepository(Utilisateur::class)->find(1);
-        
+        if (!$user) {
+            $this->addFlash('error', 'Utilisateur non trouvé.');
+            return $this->redirectToRoute('app_objet_index');
+        }
+
         // Créer un nouvel échange
         $echange = new Echange();
+        $echange->setNameEchange('Échange proposé pour ' . $objet->getNom());
+        $echange->setDateEchange(new \DateTime());
         $echange->setUtilisateur($user);
         $echange->setObjet($objet);
-        $echange->setNameEchange("Échange pour " . $objet->getNom());
-        $echange->setDateEchange(new \DateTime());
+        $echange->setImageEchange($objet->getImage() ?? '');
         $echange->setStatut('en_attente');
+
+        // Créer le formulaire
+        $form = $this->createForm(EchangeType::class, $echange, [
+            'objet_propose' => $objet,
+            'user' => $user,
+        ]);
         
-        $form = $this->createForm(EchangeType::class, $echange);
+        // Traiter la soumission du formulaire
         $form->handleRequest($request);
-        
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($echange);
             $entityManager->flush();
-            
-            // Send notification via Mercure
-            $notificationSent = $notificationService->notify(
-                'new_exchange', 
-                'Nouvelle proposition d\'échange pour: ' . $objet->getNom(),
-                [
-                    'id' => $echange->getIdEchange(),
-                    'name' => $echange->getNameEchange(),
-                    'object_id' => $objet->getIdObjet(),
-                    'object_name' => $objet->getNom(),
-                    'status' => 'en_attente'
-                ]
-            );
-            
-            // Log the notification status but don't prevent functionality
-            if (!$notificationSent) {
-                // Log that notification wasn't sent, but continue normally
-            }
 
             $this->addFlash('success', 'Votre proposition d\'échange a été envoyée avec succès !');
             return $this->redirectToRoute('app_echange_index');
         }
-        
-        return $this->render('back_office/echanges/new.html.twig', [
+
+        // Afficher le formulaire
+        return $this->render('echange/new.html.twig', [
             'form' => $form->createView(),
-            'objet' => $objet,
+            'objet_propose' => $objet
         ]);
     }
 
     #[Route('/{id_echange}', name: 'app_echange_show', methods: ['GET'])]
     public function show(Echange $echange): Response
     {
-        return $this->render('back_office/echanges/show.html.twig', [
+        // Ajouter des messages flash en fonction du statut
+        if ($echange->getStatut() === 'accepte') {
+            $this->addFlash('success', 'Votre échange a été accepté ! Vous pouvez maintenant contacter le propriétaire pour organiser l\'échange.');
+        } elseif ($echange->getStatut() === 'refuse') {
+            $this->addFlash('danger', 'Désolé, votre proposition d\'échange a été refusée.');
+        }
+
+        return $this->render('echange/show.html.twig', [
             'echange' => $echange,
         ]);
     }
@@ -85,19 +84,20 @@ class EchangeController extends AbstractController
     #[Route('/{id_echange}/edit', name: 'app_echange_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Echange $echange, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(EchangeType::class, $echange);
-        $form->handleRequest($request);
+        $form = $this->createForm(EchangeType::class, $echange, [
+            'objet_propose' => $echange->getObjet(),
+            'user' => $echange->getUtilisateur(),
+        ]);
         
+        $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
-            
-            $this->addFlash('success', 'L\'échange a été modifié avec succès !');
             return $this->redirectToRoute('app_echange_index');
         }
-        
-        return $this->render('back_office/echanges/edit.html.twig', [
-            'form' => $form->createView(),
+
+        return $this->render('echange/edit.html.twig', [
             'echange' => $echange,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -107,87 +107,9 @@ class EchangeController extends AbstractController
         if ($this->isCsrfTokenValid('delete'.$echange->getIdEchange(), $request->request->get('_token'))) {
             $entityManager->remove($echange);
             $entityManager->flush();
-            
             $this->addFlash('success', 'L\'échange a été supprimé avec succès !');
         }
-        
+
         return $this->redirectToRoute('app_echange_index');
-    }
-    
-    #[Route('/{id_echange}/accept', name: 'app_echange_accept', methods: ['POST'])]
-    public function accept(Request $request, Echange $echange, EntityManagerInterface $entityManager, NotificationService $notificationService): Response
-    {
-        // Vérifier le token CSRF
-        if ($this->isCsrfTokenValid('accept'.$echange->getIdEchange(), $request->request->get('_token'))) {
-            // Mettre à jour le statut de l'échange
-            $echange->setStatut('accepte');
-            
-            // Mettre à jour l'état de l'objet à "non disponible"
-            $objet = $echange->getObjet();
-            $objet->setEtat('attendu');
-            
-            // Persister les changements
-            $entityManager->flush();
-            
-            // Send notification via Mercure
-            $notificationSent = $notificationService->notify(
-                'exchange_accepted', 
-                'Proposition d\'échange acceptée pour: ' . $objet->getNom(),
-                [
-                    'id' => $echange->getIdEchange(),
-                    'name' => $echange->getNameEchange(),
-                    'object_id' => $objet->getIdObjet(),
-                    'object_name' => $objet->getNom(),
-                    'user_id' => $echange->getUtilisateur()->getIdUtilisateur()
-                ]
-            );
-            
-            // Log the notification status but don't prevent functionality
-            if (!$notificationSent) {
-                // Log that notification wasn't sent, but continue normally
-            }
-
-            $this->addFlash('success', 'L\'échange a été accepté avec succès !');
-        }
-        
-        return $this->redirectToRoute('app_echange_show', ['id_echange' => $echange->getIdEchange()]);
-    }
-    
-    #[Route('/{id_echange}/refuse', name: 'app_echange_refuse', methods: ['POST'])]
-    public function refuse(Request $request, Echange $echange, EntityManagerInterface $entityManager, NotificationService $notificationService): Response
-    {
-        // Vérifier le token CSRF
-        if ($this->isCsrfTokenValid('refuse'.$echange->getIdEchange(), $request->request->get('_token'))) {
-            // Mettre à jour le statut de l'échange
-            $echange->setStatut('refuse');
-            
-            // L'objet reste disponible, pas besoin de modifier son état
-            
-            // Persister les changements
-            $entityManager->flush();
-            
-            // Send notification via Mercure
-            $objet = $echange->getObjet();
-            $notificationSent = $notificationService->notify(
-                'exchange_refused', 
-                'Proposition d\'échange refusée pour: ' . $objet->getNom(),
-                [
-                    'id' => $echange->getIdEchange(),
-                    'name' => $echange->getNameEchange(),
-                    'object_id' => $objet->getIdObjet(),
-                    'object_name' => $objet->getNom(),
-                    'user_id' => $echange->getUtilisateur()->getIdUtilisateur()
-                ]
-            );
-            
-            // Log the notification status but don't prevent functionality
-            if (!$notificationSent) {
-                // Log that notification wasn't sent, but continue normally
-            }
-
-            $this->addFlash('warning', 'L\'échange a été refusé.');
-        }
-        
-        return $this->redirectToRoute('app_echange_show', ['id_echange' => $echange->getIdEchange()]);
     }
 }
--- src/Form/EchangeType.php
+++ src/Form/EchangeType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Echange;
+use App\Entity\Objet;
+use App\Entity\Utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -10,35 +13,51 @@ use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
 
 class EchangeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $objetPropose = $options['objet_propose'];
+        $user = $options['user'];
+
         $builder
             ->add('nameEchange', TextType::class, [
                 'label' => 'Titre de l\'échange',
                 'required' => true,
                 'attr' => [
-                    'placeholder' => 'Donnez un titre à votre proposition d\'échange',
-                    'class' => 'form-control'
+                    'placeholder' => 'Donnez un titre à votre proposition d\'échange'
                 ]
             ])
-            ->add('description', TextareaType::class, [
-                'label' => 'Description de votre proposition',
+            ->add('objet', EntityType::class, [
+                'class' => Objet::class,
+                'choice_label' => 'nom',
+                'label' => 'Votre objet à échanger',
+                'query_builder' => function (EntityRepository $er) use ($user) {
+                    return $er->createQueryBuilder('o')
+                        ->where('o.id_utilisateur = :user')
+                        ->andWhere('o.etat = :etat')
+                        ->setParameter('user', $user)
+                        ->setParameter('etat', 'disponible');
+                },
+                'placeholder' => 'Choisissez un de vos objets à échanger',
+                'required' => true,
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message pour le propriétaire',
                 'required' => true,
                 'attr' => [
-                    'placeholder' => 'Détaillez votre proposition d\'échange...',
-                    'rows' => 4,
-                    'class' => 'form-control'
+                    'placeholder' => 'Expliquez pourquoi vous souhaitez faire cet échange...',
+                    'rows' => 4
                 ]
             ])
             ->add('dateEchange', DateTimeType::class, [
+                'label' => 'Date souhaitée pour l\'échange',
+                'required' => true,
                 'widget' => 'single_text',
-                'data' => new \DateTime(),
-                'label' => 'Date de la proposition',
                 'attr' => [
-                    'class' => 'form-control'
+                    'min' => (new \DateTime())->format('Y-m-d\TH:i')
                 ]
             ])
             ->add('statut', ChoiceType::class, [
@@ -49,15 +68,16 @@ class EchangeType extends AbstractType
                     'Refusé' => 'refuse'
                 ],
                 'required' => true,
-                'data' => 'en_attente',
-                'attr' => [
-                    'class' => 'form-control'
-                ]
+                'data' => 'en_attente'
             ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        $resolver->setRequired(['objet_propose', 'user']);
+        $resolver->setAllowedTypes('objet_propose', Objet::class);
+        $resolver->setAllowedTypes('user', Utilisateur::class);
+        
         $resolver->setDefaults([
             'data_class' => Echange::class,
         ]);
